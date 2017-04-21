5cb5ebbdb2412d9f6107a77ddfd0ae3a74a8c14d
Thanapon Chapchainai <boatbas57150@gmail.com>
2017-04-21 18:57:11 +0700

    GH-8
    Add Unit test the strawberry class

--- farmapp/test/Missing/farmapp/model/Plant/StrawberryTest.java
+++ farmapp/test/Missing/farmapp/model/Plant/StrawberryTest.java
@@ -7,8 +7,77 @@ import Missing.farmapp.model.Plant.Strawberry;
 public class StrawberryTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TestGetName() throws Exception {
+		Strawberry s = new Strawberry();
+		assertEquals(s.getName(),"Strawberry");		
 	}
+	
+	@Test
+	public void TestSetName() throws Exception {
+		Strawberry s = new Strawberry();
+		s.setName("Papaya");
+		assertEquals(s.getName(),"Papaya");
+	}
+	
+	@Test
+	public void TestGetHealth() throws Exception {
+		Strawberry s = new Strawberry();
+		assertEquals(s.getHealth(),5);		
+	}
+	
+	@Test
+	public void TestSetHealth() throws Exception {
+		Strawberry s = new Strawberry();
+		s.setHealth(5);
+		assertEquals(s.getHealth(),5);
+	}
+
+	@Test
+	public void TestgetWaterContainer() throws Exception{
+		Strawberry s = new Strawberry();
+		assertEquals(s.getWaterContainer(),2);
+	}
+	@Test
+	public void TestsetWaterContainer() throws Exception{
+		Strawberry s = new Strawberry();
+		s.setWaterContainer(7);
+		assertEquals(s.getWaterContainer(),7);
+	}
+
+	@Test
+	public void TestgetAge() throws Exception{
+		Strawberry s = new Strawberry();
+		assertEquals(s.getAge(),0);
+	}
+	@Test
+	public void TestsetAge() throws Exception{
+		Strawberry s = new Strawberry();
+		s.setAge(1);
+		assertEquals(s.getAge(),1);
+	}
+
+	@Test
+	public void TestgetLifeCycle() throws Exception{
+		Strawberry s = new Strawberry();
+		assertEquals(s.getLifeCycle(),5);
+	}
+	@Test
+	public void TestsetLifeCycle() throws Exception{
+		Strawberry s = new Strawberry();
+		s.setLifeCycle(1);
+		assertEquals(s.getLifeCycle(),1);
+	}
+	
+	@Test
+	public void Testgettype() throws Exception{
+		Strawberry s = new Strawberry();
+		assertEquals(s.gettype(),null);
+	}
+	@Test
+	public void Testsettype() throws Exception{
+		Strawberry s = new Strawberry();
+		s.settype(null);
+		assertEquals(s.gettype(),null);
+	}	
 
 }
