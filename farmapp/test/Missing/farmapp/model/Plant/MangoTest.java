b24e8f35a5bc9a9990d8b5c267a2038e62a10a20
Thanapon Chapchainai <boatbas57150@gmail.com>
2017-04-21 18:08:24 +0700

    GH-6
    Add MangoTest Unit test of the Mango Class

--- farmapp/test/Missing/farmapp/model/Plant/MangoTest.java
+++ farmapp/test/Missing/farmapp/model/Plant/MangoTest.java
@@ -5,5 +5,79 @@ import org.junit.Test;
 import Missing.farmapp.model.Plant.Mango;
 
 public class MangoTest {
+	@Test
+	public void TestGetName() throws Exception {
+		Mango m = new Mango();
+		assertEquals(m.getName(),"Mango");		
+	}
 	
+	@Test
+	public void TestSetName() throws Exception {
+		Mango m = new Mango();
+		m.setName("Mango");
+		assertEquals(m.getName(),"Mango");
+	}
+	
+	@Test
+	public void TestGetHealth() throws Exception {
+		Mango m = new Mango();
+		assertEquals(m.getHealth(),5);		
+	}
+	
+	@Test
+	public void TestSetHealth() throws Exception {
+		Mango m = new Mango();
+		m.setHealth(5);
+		assertEquals(m.getHealth(),5);
+	}
+
+	@Test
+	public void TestgetWaterContainer() throws Exception{
+		Mango m = new Mango();
+		assertEquals(m.getWaterContainer(),7);
+	}
+	@Test
+	public void TestsetWaterContainer() throws Exception{
+		Mango m = new Mango();
+		m.setWaterContainer(7);
+		assertEquals(m.getWaterContainer(),7);
+	}
+
+	@Test
+	public void TestgetAge() throws Exception{
+		Mango m = new Mango();
+		assertEquals(m.getAge(),0);
+	}
+	@Test
+	public void TestsetAge() throws Exception{
+		Mango m = new Mango();
+		m.setAge(1);
+		assertEquals(m.getAge(),1);
+	}
+
+	@Test
+	public void TestgetLifeCycle() throws Exception{
+		Mango m = new Mango();
+		assertEquals(m.getLifeCycle(),5);
+	}
+	@Test
+	public void TestsetLifeCycle() throws Exception{
+		Mango m = new Mango();
+		m.setLifeCycle(1);
+		assertEquals(m.getLifeCycle(),1);
+	}
+	
+	@Test
+	public void Testgettype() throws Exception{
+		Mango m = new Mango();
+		assertEquals(m.gettype(),null);
+	}
+	@Test
+	public void Testsettype() throws Exception{
+		Mango m = new Mango();
+		m.settype(null);
+		assertEquals(m.gettype(),null);
+	}	
+	
+}
 }
