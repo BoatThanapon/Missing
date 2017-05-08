47c4faa3f538678823ac714e688775ca616fc381
Rawisara Loekiatthamrong <sunnymorning.12@gmail.com>
2017-05-08 15:45:48 +0700

    Remove and Replace for change project structure
    Renew Structure of project

--- farmapp/test/Missing/farmapp/controller/GameControllerTest.java
+++ farmapp/test/Missing/farmapp/controller/GameControllerTest.java
@@ -7,58 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 import Missing.farmapp.controller.GameController;
-import Missing.farmapp.model.Plant.Mango;
-import Missing.farmapp.model.Plant.Papaya;
-import Missing.farmapp.model.Plant.Plant;
-import Missing.farmapp.model.Plant.Strawberry;
-
 
 public class GameControllerTest {
-	static GameController gameController = new GameController();
-	@Test
-	public void testNextday() {
-		gameController.NextDay();
-		assertEquals(gameController.GetDay(),2);
-	}
-
-	@Test
-	public void AddPlant() {
-		List<Plant> pl = new ArrayList<Plant>();
-		Plant p = new Papaya();
-		Plant s = new Strawberry();
-		pl.add(p);
-		pl.add(s);
-		gameController.AddPlant(p);
-		gameController.AddPlant(s);
-		assertEquals(gameController.GetPlantList(), pl);
-	}
-
-	@Test
-	public void testGetPlantList() {
-		List<Plant> pl= new ArrayList<Plant>();
-		pl = gameController.GetPlantList();
-		assertEquals(gameController.GetPlantList(), pl);
-	}
-
-	@Test
-	public void testRemovePlant() {
-		List<Plant> pl = new ArrayList<Plant>();
-		Plant p = new Papaya();
-		Plant s = new Strawberry();
-		p.setAge(5);
-		p.setLifeCycle(3);
-		s.setAge(6);
-		s.setLifeCycle(5);
-		pl.add(p);
-		pl.add(s);
-		gameController.RemovePlant(pl);
-		assertEquals(gameController.RemovePlant(pl), true);
-		
-	}
-
-	@Test
-	public void testGetDay() {
-		assertEquals(gameController.GetDay(), 1);
-	}
-
+	
 }
