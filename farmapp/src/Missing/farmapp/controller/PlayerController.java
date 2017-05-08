9145d1cd5c18be3b3a1bd267b7b3a5b34d078f07
Rawisara Loekiatthamrong <sunnymorning.12@gmail.com>
2017-05-08 15:57:20 +0700

    Add PlayerController to User can view inventory
    GH-20

---
+++ farmapp/src/Missing/farmapp/controller/PlayerController.java
@@ -0,0 +1,120 @@
+package Missing.farmapp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Missing.farmapp.model.GardenBed;
+import Missing.farmapp.model.Item;
+import Missing.farmapp.model.Fruit.Fruit;
+
+public class PlayerController {
+
+	private String name;
+	private int energy;
+	private double power;
+	private int money;
+	private List<Item> inventory = new ArrayList<Item>();
+	
+	public PlayerController(){
+		name = "Doraemon";
+		energy = 30;
+		power = 0;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public double getEnergy(){
+		return energy;
+	}	
+
+	public boolean sellItem(){
+		if(inventory.size()==0){
+			System.out.println("Empty Item");
+		}else{
+			showInventory();
+		}
+		return true;
+	}
+	
+	public List<Item> getSeeds(){
+		List<Item> seeds = new ArrayList<Item>();
+		for (Item item : this.inventory) {
+				seeds.add(item);
+		}
+		return seeds;
+	}
+	
+	public void showInventory(){
+		if(inventory.size()==0){
+			System.out.println("Your inventory is empty.");
+		}else{
+		int index = 0;
+			for (Item item : inventory) {
+				index++;
+				System.out.println("[" + index + "] : "+ item.getName() +" has "+item.getAmount());
+			}
+		}
+	}
+	
+	public boolean buyItem(Item item, int amount,int price){
+		addItemToInventory(item,amount);
+		money-=price;
+		return true;
+	}
+	
+	public boolean addItemToInventory(Item item, int amount){
+		for(int i=0; i<inventory.size(); i++){
+			if(inventory.get(i).getName()==item.getName()){
+				inventory.get(i).addAmount(amount);
+				return true;
+			}
+		}
+		item.setAmount(amount);
+		inventory.add(item);
+		return true;
+	}
+	
+	public boolean useItem(int index) {
+		return true;
+	}
+	
+	public boolean useSeed(String name){
+		for(int i=0; i<inventory.size(); i++){
+			if(inventory.get(i).getName()==name){
+				if(inventory.get(i).getAmount()>0){
+					inventory.get(i).use();
+					if(inventory.get(i).getAmount()==0){
+						inventory.remove(i);
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean harvestPlant(GardenBed gardenbed){
+		boolean harvestSomething = false;
+		for(int i=0; i<gardenbed.size(); i++){
+			if(gardenbed.getPlant(i)!=null){
+				Fruit fruit = gardenbed.getPlant(i).isHarvested();
+				if(fruit!=null){
+					addItemToInventory(fruit,fruit.getAmount());
+					System.out.println("You got "+fruit.getAmount()+" of "+fruit.getName());
+					harvestSomething = true;
+				}
+			}
+		}
+		if(!harvestSomething){
+			System.out.println("Empty to harvest.");
+		}
+		return true;
+	}
+
+	public int getMoney() {
+		// TODO Auto-generated method stub
+		return money;
+	}
+}
